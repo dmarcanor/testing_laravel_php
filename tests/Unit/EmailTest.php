efe2e209cef3b01b9ebf3cc061046791d2e53bd5
Daniel Marcano <dmarcanor2@gmail.com>
2021-10-05 16:40:29 -0400

    Install project & email validate test

---
+++ tests/Unit/EmailTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Helpers\Email;
+use PHPUnit\Framework\TestCase;
+
+class EmailTest extends TestCase
+{
+
+    public function test_email()
+    {
+        $result = Email::validate('d@admin.com');
+
+        $this->assertTrue($result);
+    }
+
+    public function test_not_validate_email()
+    {
+        $result = validate_email('xd');
+
+        $this->assertFalse($result);
+    }
+}
