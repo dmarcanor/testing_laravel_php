fdb4c7e39a5edaa666448ac96e996796d0c42d89
Daniel Marcano <dmarcanor2@gmail.com>
2021-10-07 13:00:23 -0400

    Accessors and mutators

---
+++ app/Models/Post.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    use HasFactory;
+
+    public function setNameAttribute($value): void
+    {
+        $this->attributes['name'] = strtolower($value);
+    }
+
+    public function getSlugAttribute(): string
+    {
+        return str_replace(' ', '-', $this->name);
+    }
+}
