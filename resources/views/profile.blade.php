53878da3dbf13c90b89d4a271e4e33c2b9a3b019
Daniel Marcano <dmarcanor2@gmail.com>
2021-10-07 13:23:44 -0400

    Uploading files from browser

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -1,1 +1,21 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="x-ua-compatible" content="ie=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+    <title></title>
+    <link rel="stylesheet" href="css/main.css" />
+    <link rel="icon" href="images/favicon.png" />
+</head>
+
+<body>
+    <form action="profile" method="POST" enctype="multipart/form-data">
+        @csrf
+        <input type="file" name="photo">
+
+        <input type="submit" value="Enviar">
+    </form>
+</body>
+</html>
