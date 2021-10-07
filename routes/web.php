cb4271b0ed3ec75f4c47ed072136e924c69b64bd
Daniel Marcano <dmarcanor2@gmail.com>
2021-10-07 13:17:09 -0400

    Uploading files

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::view('profile', 'profile');
+
+Route::post('profile', function (\Illuminate\Http\Request $request) {
+    $request->file('photo')->store('profiles');
+
+    return redirect('profile');
+});
