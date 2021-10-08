f6a054133cd99084ac08c91e4e0c62968ade9cca
Daniel Marcano <dmarcanor2@gmail.com>
2021-10-08 09:55:10 -0400

    Testing HTTP

--- routes/web.php
+++ routes/web.php
@@ -2,16 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 Route::get('/', function () {
     return view('welcome');
@@ -20,3 +10,9 @@ Route::get('/', function () {
 Route::view('profile', 'profile');
 
 Route::post('profile', [\App\Http\Controllers\ProfileController::class, 'upload']);
+
+Route::get('/', function () {
+    return view('welcome', [
+        'tags' => \App\Models\Tag::get()
+    ]);
+});
