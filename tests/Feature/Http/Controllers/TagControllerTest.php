3bd93e3ef17d0a117144da7b73f27a25a4c2c91c
Daniel Marcano <dmarcanor2@gmail.com>
2021-10-08 10:35:31 -0400

    Validate test

--- tests/Feature/Http/Controllers/TagControllerTest.php
+++ tests/Feature/Http/Controllers/TagControllerTest.php
@@ -27,4 +27,11 @@ class TagControllerTest extends TestCase
 
         $this->assertDatabaseMissing('tags', ['name' => $tag->name]);
     }
+
+    public function test_validate()
+    {
+        $this
+            ->post('tags', ['name' => ''])
+            ->assertSessionHasErrors('name');
+    }
 }
