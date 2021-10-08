4514df5ea44498b2e1f27012eb3f77a5c199b3af
Daniel Marcano <dmarcanor2@gmail.com>
2021-10-08 10:40:34 -0400

    Unit tests for new features

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -12,4 +12,11 @@ class Tag extends Model
     protected $fillable = [
         'name'
     ];
+
+    public function getSlugAttribute(): string
+    {
+        return strtolower(
+            str_replace(' ', '-', $this->name)
+        );
+    }
 }
